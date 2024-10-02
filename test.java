5479973129b4ed7b3052bffaf4481754040bfe87
lovelybugf <nam8dmd@gmail.com>
2024-10-02 23:05:23 +0700

    test
    Please enter the commit message for your changes. Lines starting
    :wq

---
+++ test.java
@@ -0,0 +1,20 @@
+
+import java.net.IDN;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Admin
+ */
+public class test {
+    string id,ten,vo;
+    public static void main test( id, ten, vo){
+        this.id = id;
+        this.ten = ten;
+        this.vo instanceof = vlo
+    }
+}
