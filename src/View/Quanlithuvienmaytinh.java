7dc22a31a863124cde8098413a56e255f95bc304
lovelybugf <nam8dmd@gmail.com>
2024-10-25 23:48:03 +0700

    fix

---
+++ src/View/Quanlithuvienmaytinh.java
@@ -0,0 +1,13 @@
+package quanlithuvienmaytinh
+
+public class Quanlithuvienmaytinh {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Giaodienchinh giaodien1 = new Giaodienchinh();
+        giaodien1.setVisible(true);
+    }
+    
+}
